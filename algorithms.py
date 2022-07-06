321815d0fe9529cb699d039cd5bc2d8d23ed86ff
Sam Chamani <samchamani@Sams-MBP-2.fritz.box>
2022-07-06 15:47:41 +0200

    minimax

--- algorithms.py
+++ algorithms.py
@@ -3,7 +3,50 @@ from time import time
 from helpers import *
     
 def minimax(board,depth):
-    return 0
+    nextMoves = []
+    if depth <= 0:
+        return 
+    start = time()
+    isWhite = board.isWhiteTurn
+    moves = board.getMoves()
+    
+    for move in moves:
+        board.doMove(move)
+        nextMoves.append({
+            "move": move,
+            "value": minimaxAlgorithm(board, depth - 1, False, isWhite)
+        })
+        board.undoLastMove()
+    end = time()
+    print("Minimax took "+ str(end - start)+ " seconds.")
+    highestVal = max(nextMoves, key=itemgetter("value"))["value"]
+    bestMoves = []
+    for move in nextMoves:
+        if move["value"] == highestVal:
+            bestMoves.append(move)
+    return pickRandom(bestMoves)
+
+def minimaxAlgorithm(board, depth, isMax, isWhiteAtRoot):
+    if depth == 0 or board.isGameDone():
+        colorFactor = -1
+        if board.isWhiteTurn and isWhiteAtRoot:
+            colorFactor = 1
+        return colorFactor * board.evaluate() * (1.1**depth)    
+    if isMax:
+        value = float('-inf')
+        for move in board.getMoves():
+            board.doMove(move)
+            value = max(value, minimaxAlgorithm(board, depth-1, False, isWhiteAtRoot))
+            board.undoLastMove()
+        return value
+    else:
+        value = float('inf')
+        for move in board.getMoves():
+            board.doMove(move)
+            value = min(value, minimaxAlgorithm(board, depth-1, True, isWhiteAtRoot))
+            board.undoLastMove()
+        return value
+    
         
 def alphaBeta(board,depth):
     # might switch to single move approach for bigger cutoff effect
@@ -15,11 +58,14 @@ def alphaBeta(board,depth):
     moves = board.getMoves()
     for move in moves:
         board.doMove(move)
+        value = alphaBetaAlgorithm(board, depth - 1, -10000,10000, False, isWhite)
         nextMoves.append({
             "move": move,
-            "value": alphaBetaAlgorithm(board, depth - 1, -10000,10000, False, isWhite)
+            "value": value
         })
         board.undoLastMove()
+        if value >= 10000:
+            break
     end = time()
     print("AlphaBeta took "+ str(end - start)+ " seconds.")
     highestVal = max(nextMoves, key=itemgetter("value"))["value"]
